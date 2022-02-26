dd8b3fa86a85301e918cc85540ba1700f1546a15
gy <gayunlee11@gmail.com>
2022-02-26 15:17:53 +0900

    Refactor: API 엔드포인트 상수화

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
+import { KAKAO_AUTH_PAGE_URL } from './Oauth';
 
-const CLIENT_ID = process.env.REACT_APP_CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:3000/signin_kakao';
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 function Login() {
   return (
     <Container>
@@ -19,7 +17,7 @@ function Login() {
           <LoginInput placeholder="이메일 아이디" />
           <LoginInput placeholder="비밀번호" />
           <LoginButton>LOGIN</LoginButton>
-          <KakaoBtn href={KAKAO_AUTH_URL}>
+          <KakaoBtn href={KAKAO_AUTH_PAGE_URL}>
             <img src="/images/kakaoBtn.png" alt="카카오 로그인 버튼" />
           </KakaoBtn>
         </LoginForm>
--- src/pages/Login/Oauth.js
+++ src/pages/Login/Oauth.js
@@ -1,4 +1,8 @@
-const CLIENT_ID = process.env.REACT_APP_CLIENT_SECRET;
+export const API_KEY = process.env.REACT_APP_API_KEY;
+export const CLIENT_SECRET_KEY = process.env.REACT_APP_CLIENT_SECRET;
 const REDIRECT_URI = 'http://localhost:3000/signin_kakao';
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_PAGE_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_SECRET_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
+export const JWT_API_URL =
+  'http://ec2-3-36-124-170.ap-northeast-2.compute.amazonaws.com/users/signin-kakao';
