df1c2655cda49ef9ac6b7868d4b905c1fa928b23
Gayun00 <gayunlee11@gmail.com>
2022-03-25 18:19:30 +0900

    Add: useQueryString 커스텀 훅 테스트 추가

---
+++ src/utils/hooks/useQueryString.test.js
@@ -0,0 +1,73 @@
+/**
+ * @jest-environment jsdom
+ */
+
+describe('useQueryStringObject', () => {
+  let testobj;
+  let testObjWithArr;
+  let search;
+  let parseObjectToSearchParams;
+  let parseQueryIntoObject;
+  let state;
+  let URLSearch;
+
+  beforeEach(() => {
+    testobj = {
+      checkin: '2022-03-22',
+      checkout: '2022-03-26',
+    };
+
+    testObjWithArr = {
+      category: ['guesthouse', 'hotel'],
+    };
+
+    search = '';
+    URLSearch = new URLSearchParams(search);
+
+    parseObjectToSearchParams = (obj = state, page = 'list') => {
+      let updatedValue;
+
+      Object.entries(obj).map(([key, value]) => {
+        updatedValue = Array.isArray(value) ? value.join('&') : value;
+
+        value && URLSearch.set(key, updatedValue);
+      });
+      return `/${page}?` + URLSearch.toString();
+    };
+
+    parseQueryIntoObject = querystring => {
+      const params = new URLSearchParams(querystring);
+      const obj = {};
+
+      for (const key of params.keys()) {
+        if (params.getAll(key).length > 1) {
+          obj[key] = params.getAll(key);
+        } else {
+          obj[key] = params.get(key);
+        }
+      }
+
+      return obj;
+    };
+  });
+
+  it('객체를 쿼리스트링으로 변환', () => {
+    expect(parseObjectToSearchParams(testobj)).toBe(
+      '/list?checkin=2022-03-22&checkout=2022-03-26'
+    );
+  });
+
+  it('쿼리스트링을 객체로 변환', () => {
+    expect(
+      parseQueryIntoObject(`checkin=2022-03-22&checkout=2022-03-26`)
+    ).toEqual({
+      checkin: '2022-03-22',
+      checkout: '2022-03-26',
+    });
+  });
+  it('배열을 쿼리스트링으로 변환', () => {
+    expect(parseObjectToSearchParams(testObjWithArr)).toBe(
+      `/list?category=guesthouse%26hotel`
+    );
+  });
+});
