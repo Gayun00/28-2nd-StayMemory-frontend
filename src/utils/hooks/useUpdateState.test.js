ade316fb27054cd0f57c00927c23a86c02b59ef3
Gayun00 <gayunlee11@gmail.com>
2022-03-25 18:20:08 +0900

    Add: useUpdateState 커스텀 훅 추가 - useQueryStringArr 에서 관심사 분리 - 객체 혹은 배열 상태 변경 로직 분리

---
+++ src/utils/hooks/useUpdateState.test.js
@@ -0,0 +1,67 @@
+/**
+ * @jest-environment jsdom
+ */
+
+describe('객체와 배열 상태 변경 테스트', () => {
+  let updateState;
+  let testObj;
+  let testObjWithArr;
+  let state;
+
+  beforeEach(() => {
+    updateState = (key, value) => {
+      if (Array.isArray(state[key])) {
+        let updatedArray = [];
+        let updatedState = [];
+        if (!state[key].includes(value)) {
+          updatedArray = [...state[key], value];
+        } else {
+          updatedArray = [...state[key]].filter(el => {
+            return el !== value;
+          });
+        }
+        updatedState = {
+          [key]: updatedArray,
+        };
+        return updatedState;
+      } else {
+        let updatedState = {};
+        updatedState = {
+          ...state,
+          [key]: value,
+        };
+        return updatedState;
+      }
+    };
+    testObj = {
+      checkin: '2022-03-22',
+      checkout: '2022-03-26',
+    };
+
+    testObjWithArr = {
+      category: ['guesthouse'],
+    };
+  });
+
+  it('state가 객체일 떄 전달받은 key와 value로 상태변경', () => {
+    state = testObj;
+    expect(updateState('checkin', '2022-03-24')).toEqual({
+      checkin: '2022-03-24',
+      checkout: '2022-03-26',
+    });
+  });
+
+  it('변경할 state가 배열일 때 중복되는 값이 없으면 전달받은 value 추가', () => {
+    state = testObjWithArr;
+    expect(updateState('category', 'hotel')).toEqual({
+      category: ['guesthouse', 'hotel'],
+    });
+  });
+
+  it('변경할 state가 배열일 때 중복되는 값이 있으면 전달받은 value 삭제', () => {
+    state = testObjWithArr;
+    expect(updateState('category', 'guesthouse')).toEqual({
+      category: [],
+    });
+  });
+});
